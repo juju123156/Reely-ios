898c7066e5d8e9d3838d5352817fbdf457d45359
pearyoon <yooon0769@gmail.com>
2024-12-09 21:31:11 +0900

    navigation 구조 변경

--- Reely/app/App.tsx
+++ Reely/app/App.tsx
@@ -1,22 +1,12 @@
-// App.js
+// App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './components/member/Login';  // 로그인 화면
-import MainScreen from './components/common/Main';    // 홈 화면
+import AppNavigation from './components/navigations/AppNavigation';
 
-const Stack = createStackNavigator();
-
-const App = () => {
+const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }} // 여기서 모든 스크린에 대해 헤더를 숨길 수 있습니다.
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Main" component={MainScreen} />
-      </Stack.Navigator>
+      <AppNavigation />
     </NavigationContainer>
   );
 };
